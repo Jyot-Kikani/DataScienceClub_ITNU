272671ff65d45d79c19268a399ec66083ce2a1db
shlokkvaishnav <shlok9640@gmail.com>
2025-10-20 16:15:39 +0530

    fixed theme issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Navigation } from "@/components/Navigation";
 import { Footer } from "@/components/Footer";
@@ -12,31 +13,44 @@ import About from "./pages/About";
 import Events from "./pages/Events";
 import Resources from "./pages/Resources";
 import Team from "./pages/Team";
-
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
+// ✅ Smooth scroll to top whenever route changes
+function ScrollToTopOnRouteChange() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+  return null;
+}
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       <TooltipProvider>
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          {/* 👇 Add scroll-to-top listener */}
+          <ScrollToTopOnRouteChange />
+
           <AnimatedCursor />
           <Navigation />
+
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/about" element={<About />} />
             <Route path="/events" element={<Events />} />
             <Route path="/resources" element={<Resources />} />
-          <Route path="/team" element={<Team />} />
+            <Route path="/team" element={<Team />} />
             <Route path="/contact" element={<Contact />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            {/* Catch-all route */}
             <Route path="*" element={<NotFound />} />
           </Routes>
+
           <Footer />
         </BrowserRouter>
       </TooltipProvider>
@@ -45,4 +59,3 @@ const App = () => (
 );
 
 export default App;
-
